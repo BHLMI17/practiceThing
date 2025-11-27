6d26f53ab468309ca4c82cb83396f8dd58ec0f71
BHLMI17 <bilalhussain.lmi@gmail.com>
2025-11-27 20:57:15 +0000

    still planning

--- character.java
+++ character.java
@@ -1,4 +1,19 @@
 public class character{
-    public String name;
-    public int level;
+    private String name;
+    private int level;
+    private int exp;
+    private int nextXP;
+
+
+    public void setNameOfCharacter(String selectedName){
+        selectedName = name;
+    }
+
+    public void levelUp(){
+        if(exp >= nextXP){
+            level +=1;
+        }
+    }
+
+    
 }
